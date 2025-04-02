17027814e04101b1f1e9169ccb4a98c162b54909
mishabogdanov5 <mishab1602@gmail.com>
2025-04-02 14:10:30 +0300

    fix: add info about license to .ml files

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,3 @@
+(** Copyright 2025-2025, mishabogdanov5 *)
+
 let () = print_endline "Hello, World!"
--- lib/parser.ml
+++ lib/parser.ml
@@ -1,2 +1,4 @@
+(** Copyright 2025-2025, mishabogdanov5 *)
+
 let rec fact n = if n < 2 then 1 else n * fact (n-1)
 
