44c65e2a18f392078a084983be711b29246a1839
mishabogdanov5 <mishab1602@gmail.com>
2025-04-02 14:03:12 +0300

    Initiall commit

---
+++ test/test_parser.ml
@@ -0,0 +1,14 @@
+open Alcotest
+
+
+let test_fact ()= 
+  check int "default value" 720 (Parser.fact 6);
+  check int "one" 1 (Parser.fact 1);
+  check int "zero" 1 (Parser.fact 0)
+
+let () = 
+  run "Parser tests" [
+      ("factorial", [
+          test_case "Factorial of integer" `Quick test_fact;
+      ]);
+  ]
