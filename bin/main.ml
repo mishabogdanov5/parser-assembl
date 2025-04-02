8d2f8ea07cd56a0925a55a2804d052e1b09d4000
mishabogdanov5 <mishab1602@gmail.com>
2025-04-02 14:14:56 +0300

    fix: add license info to .ml files

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,4 @@
 (** Copyright 2025-2025, mishabogdanov5 *)
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
 let () = print_endline "Hello, World!"
--- lib/parser.ml
+++ lib/parser.ml
@@ -1,4 +1,5 @@
 (** Copyright 2025-2025, mishabogdanov5 *)
+ (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
-let rec fact n = if n < 2 then 1 else n * fact (n-1)
+ let rec fact n = if n < 2 then 1 else n * fact (n-1)
 
