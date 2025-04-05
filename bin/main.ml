e2859359bdb7b80cef7d69b6a915282e826bae64
mishabogdanov5 <mishab1602@gmail.com>
2025-04-06 02:58:40 +0300

    feat: generate assembler prog

--- bin/main.ml
+++ bin/main.ml
@@ -4,7 +4,7 @@
 
 open Parser
 
-(*let lw = Printf.sprintf "lw %s, %s\n"
+let lw = Printf.sprintf "lw %s, %s\n"
 let li = Printf.sprintf "li %s, %d\n"
 let push = Printf.sprintf "addi sp, sp, -4\nsw %s, 0(sp)\n"
 
@@ -12,27 +12,71 @@ let binop op =
   Printf.sprintf
     "lw t0, 0(sp)\nlw t1, 4(sp)\n%s t2, t1, t0\naddi sp, sp, 4\nsw t2, 0(sp)\n"
     op
-*)
 
-let rec expr_to_string = function
-  | Const c -> string_of_int c
-  | Var x -> x
-  | Add (x, y) -> "(" ^ expr_to_string x ^ "+" ^ expr_to_string y ^ ")"
-  | Sub (x, y) -> "(" ^ expr_to_string x ^ "-" ^ expr_to_string y ^ ")"
-  | Multi (x, y) -> "(" ^ expr_to_string x ^ "*" ^ expr_to_string y ^ ")"
-  | Div (x, y) -> "(" ^ expr_to_string x ^ "/" ^ expr_to_string y ^ ")"
+let generate_expr expr =
+  let rec helper = function
+    | Add (e1, e2) ->
+        Printf.sprintf "%s%s%s" (helper e1) (helper e2) (binop "add")
+    | Sub (e1, e2) ->
+        Printf.sprintf "%s%s%s" (helper e1) (helper e2) (binop "sub")
+    | Multi (e1, e2) ->
+        Printf.sprintf "%s%s%s" (helper e1) (helper e2) (binop "mul")
+    | Div (e1, e2) ->
+        Printf.sprintf "%s%s%s" (helper e1) (helper e2) (binop "div")
+    | Const n -> li "t0" n ^ push "t0"
+    | Var v -> lw "t0" v ^ push "t0"
+  in
+  helper expr
 
-let print_prog (body, params, main_args) =
-  Printf.printf "%s\n" (expr_to_string body);
-  List.iter (fun s -> Printf.printf "%s " s) params;
-  List.iter (fun e -> Printf.printf "%s " (expr_to_string e)) main_args
+let generate_asm_prog (body, params, main_args) =
+  let asm_expr = generate_expr body in
+  let const_into_nums =
+    List.map
+      (fun e ->
+        match e with Const n -> n | _ -> failwith "Error: wrong args in main")
+      main_args
+  in
+  let asm_args =
+    List.map2
+      (fun v n -> Printf.sprintf "%s: .word %d" v n)
+      params const_into_nums
+  in
+  let template =
+    Printf.sprintf
+      ".section .data\n\
+      \ .align 4\n\
+       %s\n\
+       .section .text\n\
+       .globl _start\n\
+       _start:\n\
+       %s\n\
+       addi sp, sp, 4\n\
+       mv a0, t2\n\
+       call print_int\n\
+       li a0, 0\n\
+       li a7, 93\n\
+       ecall\n"
+  in
+  template (String.concat "\n" asm_args) asm_expr
 
-let print_res str =
-  match parse_program str with
-  | Ok res -> print_prog res
-  | Error e -> Printf.printf "Error: %s" e
+let write_to_file filename str =
+  let oc = open_out filename in
+  Printf.fprintf oc "%s" str;
+  close_out oc
 
 let () =
-  let inp1 = read_line () in
-  let inp2 = read_line () in
-  print_res (inp1 ^ "\n" ^ inp2)
+  let input =
+    if Array.length Sys.argv < 2 then exit 1
+    else
+      let filename = Sys.argv.(1) in
+      let ic = open_in filename in
+      let input = really_input_string ic (in_channel_length ic) in
+      close_in ic;
+      input
+  in
+  let parsed_expr = parse_program input in
+  match parsed_expr with
+  | Ok res ->
+      let code = generate_asm_prog res in
+      write_to_file "output.s" code
+  | Error _ -> print_string "djfakjfd"
